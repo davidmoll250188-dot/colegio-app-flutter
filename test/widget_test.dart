28c1381027e05cc225f55f8565706ac7bdcf8690
David <davidmoll250188@gmail.com.com>
2026-01-14 18:32:02 -0500

    CREANDO carpeta android nueva y limpia

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:datacolegio/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ColegioApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
